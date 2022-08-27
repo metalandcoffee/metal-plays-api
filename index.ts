16fb785d606d292edafa4c4af459887e76528192
Ebonie Butler <ebonie@yikesinc.com>
2022-08-27 13:06:05 -0400

    reorganizing and mongodb dep

--- index.ts
+++ index.ts
@@ -4,6 +4,14 @@ import fetch from 'node-fetch';
 import { Buffer } from 'buffer';
 import * as fs from 'fs';
 
+// Import environment variables.
+dotenv.config();
+
+// Express server instance.
+const app = express();
+const port = 8888;
+let state = null;
+
 /**
  * Generates a random string containing numbers and letters
  * @param  {number} length The length of the string
@@ -18,76 +26,125 @@ const generateRandomString = (length) => {
 	return text;
 };
 
-// Import environment variables.
-dotenv.config();
+/**
+ * 
+ * @param res 
+ * @param params 
+ * @returns 
+ */
+const getTokenFromSpotify = async (res, params) => {
+	// Get access token from Spotify.
+	const response = await fetch('https://accounts.spotify.com/api/token', {
+		method: 'post',
+		body: params,
+		headers: {
+			'Authorization': `Basic ${Buffer.from(process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET).toString('base64')}`,
+		},
+	});
+	if (200 !== response.status) {
+		res.send(`Status: ${response.status}. Try Again Later.`);
+	}
 
-// Express server instance.
-const app = express();
-const port = 8888;
-let state = null;
+	const data = await response.json() as {
+		access_token: string,
+		token_type: string,
+		refresh_token: string,
+	};
+
+	const { access_token, token_type, refresh_token } = data;
+
+	return { access_token, token_type, refresh_token };
+}
+
+
 
 // Index page route handler.
 app.get('/', (req, res) => {
 	res.send('Welcome to Metal Plays API!');
 });
 
-// Currently playing handler.
+/**
+ * Currently Playing Track route handler.
+ */
 app.get('/api/current', (req, res) => {
-	res.send('Welcome to Metal Plays API!');
+	res.send('Under construction...');
 });
 
-// Recently played handler.
+/**
+ * Recently Played Tracks route handler.
+ */
 app.get('/api/played', async (req, res) => {
-	try {
-		// @todo read access from database versus file
-		const data = fs.readFileSync('secrets.json', 'utf8');
-		const dataObj = JSON.parse(data) as { access_token: string, token_type: string };
-		const { access_token, token_type } = dataObj;
 
-		// Get information from Spotify using access token!
-		const response = await fetch('https://api.spotify.com/v1/me/player/recently-played', {
-			headers: { Authorization: `${token_type} ${access_token}` }
-		});
-		const spotifyJSON = await response.json() as { items: any[] };
+	// Grab access token from file.
+	const data = fs.readFileSync('secrets.json', 'utf8');
+	const dataObj = JSON.parse(data) as { access_token: string, token_type: string };
+	const { access_token, token_type } = dataObj;
 
-		// Format the data to look pretty.
-		const tracks: Array<{
-			track: {
-				album: {
-					artists: Array<{
-						name: string,
-					}>
-				},
-				name: string,
-				preview_url: string,
-			},
-			name: string,
-			played_at: string,
-			context: {
-				external_urls: { spotify: string }
-			}
-		}> = spotifyJSON.items;
+	// Get information from Spotify using access token.
+	const response = await fetch('https://api.spotify.com/v1/me/player/recently-played', {
+		headers: { Authorization: `${token_type} ${access_token}` }
+	});
+	const spotifyJSON = await response.json() as {
+		items: any[],
+		error: {
+			status: number,
+			message: string
+		}
+	};
 
-		const formattedJSON = tracks.map( track => {
-			return {
-				artists: track.track.album.artists.map(artist => artist.name),
-				trackName: track.track.name,
-				playtime: track.played_at,
-				songPreviewURL: track.track.preview_url,
-				spotifyURL: track.context.external_urls.spotify,
-			};
-		});
-		res.send(formattedJSON);
-		
-	} catch (err) {
-		console.error(err);
+	// If token is expired...
+	// { error: { status: 401, message: 'The access token expired' } }
+	if (spotifyJSON.error) {
+		// @todo request a new token.
+		// call refresh_token route with refresh token URL param
+		const params = new URLSearchParams(
+			{
+				grant_type: 'refresh_token',
+				refresh_token: process.env.SPOTIFY_REFRESH_TOKEN,
+			}).toString();
+		const data = await getTokenFromSpotify(res, params) as {
+			access_token: string,
+			token_type: string,
+			refresh_token: string,
+		};
+
+		const { access_token, token_type, refresh_token } = data;
+
 	}
 
+	// Format the data to look pretty.
+	const tracks: Array<{
+		track: {
+			album: {
+				artists: Array<{
+					name: string,
+				}>
+			},
+			name: string,
+			preview_url: string,
+		},
+		name: string,
+		played_at: string,
+		context: {
+			external_urls: { spotify: string }
+		}
+	}> = spotifyJSON.items;
+
+	const formattedJSON = tracks.map(track => {
+		return {
+			artists: track.track.album.artists.map(artist => artist.name),
+			trackName: track.track.name,
+			playtime: track.played_at,
+			songPreviewURL: track.track.preview_url,
+			spotifyURL: track.context.external_urls.spotify,
+		};
+	});
+	res.send(formattedJSON);
 });
 
 // Login page route handler.
 app.get('/login', (req, res) => {
-	res.send('Login endpoint disabled');
+	//res.send('Login endpoint disabled');
 
 	const scope = 'user-read-currently-playing user-read-recently-played';
 	state = generateRandomString(16);
@@ -116,26 +173,19 @@ app.get('/callback', async (req, res) => {
 	params.append('code', code);
 	params.append('redirect_uri', process.env.SPOTIFY_REDIRECT_URI);
 
-	// Get access token from Spotify.
-	const response = await fetch('https://accounts.spotify.com/api/token', {
-		method: 'post',
-		body: params,
-		headers: {
-			'Authorization': `Basic ${Buffer.from(process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET).toString('base64')}`,
-		},
-	});
-	if (200 !== response.status) {
-		res.send(`Status: ${response.status}. Try Again Later.`);
-		//return;
-	}
-	const data = await response.json() as { access_token: string, token_type: string };
+	const data = await getTokenFromSpotify(res, params) as {
+		access_token: string,
+		token_type: string,
+		refresh_token: string,
+	};
 
-	const { access_token, token_type } = data;
+	const { access_token, token_type, refresh_token } = data;
 
 	// @todo: Temp dev code. Remove for production.
 	const content = JSON.stringify({
 		access_token,
-		token_type
+		token_type,
+		refresh_token
 	});
 	fs.writeFile('secrets.json', content, err => {
 		if (err) {
@@ -143,37 +193,6 @@ app.get('/callback', async (req, res) => {
 		}
 	});
 	res.send('Stuff happened.');
-});
-
-// Spotify refresh token route handler.
-app.get('/refresh_token', async (req, res) => {
-	// @todo verify request is coming from front-end app
-	// Extract URL Params.
-	const { refresh_token } = req.query;
-
-	// Create fetch requests parameters.
-	const params = new URLSearchParams();
-	params.append('grant_type', 'refresh_token');
-	params.append('refresh_token', refresh_token);
-	params.append('redirect_uri', process.env.SPOTIFY_REDIRECT_URI);
-
-	// Get access token from Spotify.
-	const response = await fetch('https://accounts.spotify.com/api/token', {
-		method: 'post',
-		body: params,
-		headers: {
-			'Authorization': `Basic ${Buffer.from(process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET).toString('base64')}`,
-		},
-	});
-	console.log(response.status);
-	if (200 !== response.status) {
-		res.send(`Status: ${response.status}. Try Again Later.`);
-		return;
-	}
-	const data = await response.json() as { access_token: string, token_type: string };
-	const { access_token, token_type } = data;
-	// spotify_access_token = access_token;
-	// spotify_token_type = token_type;
 });
 
 // Tell Express to listen for a connection on the specified port.
